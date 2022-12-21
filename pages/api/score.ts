572371c5d7d53e48d8ff4b09cdf0e8e2338be48c
bannzai <kingkong999yhirose@gmail.com>
2022-12-21 12:25:17 +0900

    Add score api

---
+++ pages/api/score.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export type NameResponse =
+  | {
+      result: "success";
+      prompt: string;
+      rawCompletion: string;
+      fullText: string;
+    }
+  | {
+      result: "failure";
+      message: string;
+      extension?: {
+        body: any;
+        prompt: string;
+        rawCompletion?: string;
+      };
+    };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<NameResponse>
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({
+      result: "failure",
+      message: "Only POST request allowed",
+    });
+    return;
+  }
+
+  const prompt = `マッチングアプリでマッチした女性へ最初に送るメッセージが「${req.body}」の場合の点数は`;
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: prompt,
+    max_tokens: 25,
+    best_of: 1,
+    suffix: "点です！",
+  });
+  const rawCompletion = completion.data.choices[0].text;
+
+  if (rawCompletion == null) {
+    res.status(200).json({
+      result: "failure",
+      message: "あだ名が検出できませんでした",
+      extension: {
+        body: req.body,
+        prompt,
+        rawCompletion,
+      },
+    });
+  } else {
+    res.status(200).json({
+      result: "success",
+      prompt,
+      rawCompletion,
+      fullText: prompt + rawCompletion,
+    });
+  }
+}
